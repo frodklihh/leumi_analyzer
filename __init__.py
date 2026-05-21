0bdb9a52fe32ebf2109e310192de49c411dd1420
frodklihm <mfrenklah@gmail.com>
2026-05-21 23:14:43 +0300

    Add categorization and reporting modules; implement transaction parsing and HTML report generation

---
+++ __init__.py
@@ -0,0 +1,3 @@
+from importer import load_file, parse_bank_statement
+from .categorizer import categorize
+from .reporter import filter_transactions, combined_summary, save_html_report, print_unknown_transactions
